8c402ff7ae1218e22af0c04d1c5b397778b30355
kangaroomeat <121744710+kangaroomeat@users.noreply.github.com>
2026-05-04 19:29:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+var answer = document.querySelector(".answer-box");
+var button = document.querySelector(".check-answer");
+var correct = document.querySelector(".correct");
+var incorrect = document.querySelector(".incorrect");
+
+
+button.onclick=function() {
+
+    if(answer.value === "4") {
+    console.log("correct");
+    correct.style.display = "block";
+    incorrect.style.display = "none";
+
+} else {
+    console.log("wrong");
+    incorrect.style.display = "block";
+    correct.style.display = "none";
+}
+
+}
+
