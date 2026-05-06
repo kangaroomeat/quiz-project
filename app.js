3972c2ced37f23a373797df3e6dac93206d769a7
kangaroomeat <121744710+kangaroomeat@users.noreply.github.com>
2026-05-06 16:30:15 -0500

    added error

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var answer = document.querySelector(".answer-box");
 var button = document.querySelector(".check-answer");
 var correct = document.querySelector(".correct");
 var incorrect = document.querySelector(".incorrect");
+var error = document.querySelector(".error");
 
 
 button.onclick=function() {
@@ -11,6 +12,7 @@ button.onclick=function() {
 
     if(answer.value == "") {
         console.log("must type a response")
+        error.style.display = "block";
         correct.style.display = "none";
         incorrect.style.display = "none";
         throw console.error("empty field");
@@ -18,11 +20,13 @@ button.onclick=function() {
     } 
     else if(answer.value === "4") {
     console.log("correct");
+    error.style.display = "none";
     correct.style.display = "block";
     incorrect.style.display = "none";
 
 } else {
     console.log("wrong");
+    error.style.display = "none";
     incorrect.style.display = "block";
     correct.style.display = "none";
 }
