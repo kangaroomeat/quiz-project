0a3953720d6127db7656c3e10ef86db1ce37b03f
kangaroomeat <121744710+kangaroomeat@users.noreply.github.com>
2026-05-05 21:23:00 -0500

    updated

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ var incorrect = document.querySelector(".incorrect");
 
 button.onclick=function() {
 
-    if(answer.value === "4") {
+
+    if(answer.value == "") {
+        console.log("must type a response")
+        correct.style.display = "none";
+        incorrect.style.display = "none";
+        throw console.error("empty field");
+        
+    } 
+    else if(answer.value === "4") {
     console.log("correct");
     correct.style.display = "block";
     incorrect.style.display = "none";
